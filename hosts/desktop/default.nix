11d021bc16648ecd6baf43bc7414ba921799313b
tyrellw3llick <enriquemartin1402@gmail.com>
2025-06-25 01:26:24 +0200

    update flake.lock

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -62,6 +62,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
 	nix.settings.experimental-features = [ "flakes" "nix-command" ];
+
   nix.settings.extra-substituters = [
     "https://hyprland.cachix.org/"
     "https://nix-community.cachix.org/"
@@ -70,6 +71,7 @@
 		"https://walker.cachix.org"
 		"https://walker-git.cachix.org"
   ];
+
 	nix.settings.extra-trusted-public-keys = [
 		"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
 		"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
