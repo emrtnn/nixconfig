1cebe0868667fefd0e17837a2fe48a25d0d06dcf
tyrellw3llick <enriquemartin1402@gmail.com>
2025-07-23 15:41:50 +0200

    add node + nextjs flake template

--- templates/nextjs/node/flake.nix
+++ templates/nextjs/node/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/x86_64-linux";
   };
 
   outputs = {
@@ -19,11 +20,11 @@
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
-          bun
+          nodejs_20
+          nodePackages_latest.pnpm
         ];
-
         shellHook = ''
-          echo "✅ Bun + Next.js environment ready."
+          echo "✅ Node + Next.js environment ready."
           nu
         '';
       };
