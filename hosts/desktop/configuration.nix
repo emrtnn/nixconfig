71f53d93470a833adedbebbebf88ecdc981c1035
emrtnn <enriquemartin1402@gmail.com>
2026-05-18 15:55:24 +0200

    Add hyprland cachix keys to desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -217,12 +217,14 @@
         "https://devenv.cachix.org"
         "https://noctalia.cachix.org"
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
 
       extra-trusted-public-keys = [
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
