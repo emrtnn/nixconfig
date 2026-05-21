5f7ba96dfd4eb1a0a4b77499600bc0ea033d6dfc
emrtnn <enriquemartin1402@gmail.com>
2026-05-21 12:42:50 +0200

    Add clipboard yank and paste bindings

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -1,1 +1,10 @@
+local keymap = vim.keymap.set
+
+-- Options
 vim.g.mapleader = " "
+
+-- Clipboard
+keymap({'n','v'}, '<leader>y', [["+y]])
+keymap({'n','v'}, '<leader>p', [["+p]])
+keymap({'n'}, '<leader>P', [["+P]])
+
