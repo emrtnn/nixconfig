fcd1b887bc2c94cb8becff154ee261ee0c9302fa
emrtnn <enriquemartin1402@gmail.com>
2026-05-21 10:23:32 +0200

    Add oil nvim

---
+++ dotfiles/nvim/lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      columns = {
+        'icon',
+        {'size', highlight = 'Special', align = 'right'},
+      },
+      skip_confirm_for_simple_edits = true,
+    },
+    keys = {
+      { '<leader>e', '<CMD>Oil<CR>', { desc = "Open Oil" } },
+    },
+    dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+  }
+}
