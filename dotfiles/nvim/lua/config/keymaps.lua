2b5ee306d41dc3e85e966e59f8c287706a397701
emrtnn <enriquemartin1402@gmail.com>
2026-05-21 14:13:20 +0200

    Add buffer navigation keybinds

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -8,3 +8,7 @@ keymap({'n','v'}, '<leader>y', [["+y]])
 keymap({'n','v'}, '<leader>p', [["+p]])
 keymap({'n'}, '<leader>P', [["+P]])
 
+-- Buffer Navigation
+keymap("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+keymap("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
