84586bf83e6665e0dc0dd873a3401c9e0db08a4d
emrtnn <enriquemartin1402@gmail.com>
2026-05-22 17:51:52 +0200

    Add treesitter text objects to nvim

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -37,4 +37,74 @@ return {
 			}
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			vim.g.no_plugin_maps = true
+		end,
+		config = function()
+			-- 1. Setup the behaviors
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+				},
+			})
+
+			-- Select
+			local select = require("nvim-treesitter-textobjects.select")
+			local map = vim.keymap.set
+
+			map({ "x", "o" }, "af", function()
+				select.select_textobject("@function.outer", "textobjects")
+			end, { desc = "Select outer function" })
+
+			map({ "x", "o" }, "if", function()
+				select.select_textobject("@function.inner", "textobjects")
+			end, { desc = "Select inner function" })
+
+			map({ "x", "o" }, "ac", function()
+				select.select_textobject("@class.outer", "textobjects")
+			end, { desc = "Select outer class" })
+
+			map({ "x", "o" }, "ic", function()
+				select.select_textobject("@class.inner", "textobjects")
+			end, { desc = "Select inner class" })
+
+			map({ "x", "o" }, "as", function()
+				select.select_textobject("@local.scope", "locals")
+			end, { desc = "Select language scope" })
+
+			-- Move
+			local move = require("nvim-treesitter-textobjects.move")
+
+			map({ "n", "x", "o" }, "]f", function()
+				move.goto_next_start("@function.outer")
+			end, { desc = "Next function start" })
+
+			map({ "n", "x", "o" }, "[f", function()
+				move.goto_previous_start("@function.outer")
+			end, { desc = "Previous function start" })
+
+			map({ "n", "x", "o" }, "]F", function()
+				move.goto_next_end("@function.outer")
+			end, { desc = "Next function end" })
+
+			map({ "n", "x", "o" }, "[F", function()
+				move.goto_previous_end("@function.outer")
+			end, { desc = "Previous function end" })
+
+			map({ "n", "x", "o" }, "]C", function()
+				move.goto_next_end("@class.outer")
+			end, { desc = "Next class start" })
+
+			map({ "n", "x", "o" }, "[C", function()
+				move.goto_previous_end("@class.outer")
+			end, { desc = "Previous class start" })
+		end,
+	},
 }
