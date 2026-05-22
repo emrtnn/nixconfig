7c2f0106fb8ec27c937c0903fa5299800055c90a
emrtnn <enriquemartin1402@gmail.com>
2026-05-22 13:00:01 +0200

    Add trouble nvim floating window configuration

---
+++ dotfiles/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,40 @@
+return {
+	"folke/trouble.nvim",
+	opts = {
+    keys = {
+      ["<cr>"] = "jump_close",
+      ["o"] = "jump",
+    },
+    auto_close = true,
+    focus = true,
+		modes = {
+			-- Custom mode to make Trouble behave like a floating popup
+			diagnostics_float = {
+				mode = "diagnostics",
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.7,
+				},
+				win = {
+					type = "float",
+					relative = "editor",
+					border = "rounded",
+					title = " Diagnostics",
+					title_pos = "center",
+					position = { 0.5, 0.2 },
+					size = { width = 0.4, height = 0.8 },
+				},
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics_float toggle filter.buf=0<cr>",
+			desc = "Floating Diagnostics",
+		},
+		{ "<leader>xX", "<cmd>Trouble diagnostics_float toggle focus=true<cr>", desc = "Floating Diagnostics" },
+	},
+}
