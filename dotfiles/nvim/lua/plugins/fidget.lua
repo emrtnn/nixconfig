f8be16052344758888d195279f25dc2c13a674ef
emrtnn <enriquemartin1402@gmail.com>
2026-05-22 13:24:33 +0200

    Add fidget nvim plugin for loading statuses

---
+++ dotfiles/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "j-hui/fidget.nvim",
+    opts = {
+      progress = {
+        display = {
+          done_ttl = 3,
+          done_icon = "✔",
+        },
+        notification = {
+        window = {
+          winblend = 0,
+          border = "rounded",
+        },
+      },
+      }
+    },
+  }
+}
