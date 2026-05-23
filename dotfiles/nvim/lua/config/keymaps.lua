95190fe4e933a05ef94fa75b401a31325972fd14
emrtnn <enriquemartin1402@gmail.com>
2026-05-23 23:51:19 +0200

    Add bufferline plugin

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -8,10 +8,5 @@ keymap({ "n", "v" }, "<leader>y", [["+y]])
 keymap({ "n", "v" }, "<leader>p", [["+p]])
 keymap({ "n" }, "<leader>P", [["+P]])
 
--- Buffer Navigation
-keymap("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-keymap("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-keymap("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
-
 -- Search
 keymap("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
---
+++ dotfiles/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "<leader>bd", "<cmd>bdlete<cr>", desc = "Delete This Buffer" },
+		},
+		opts = {
+			options = {
+				indicator = {
+					style = "icon",
+				},
+				buffer_close_icon = "󰅖",
+				modified_icon = "● ",
+				close_icon = " ",
+				left_trunc_marker = " ",
+				right_trunc_marker = " ",
+				diagnostics = "nvim_lsp",
+				color_icons = true,
+				separator_style = "slant", -- "slant" | "slope" | "thick" | "thin"
+			},
+		},
+	},
+}
