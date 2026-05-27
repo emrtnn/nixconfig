1af90efeb7f4f9befba4acccb8f52f1c219efbd9
emrtnn <enriquemartin1402@gmail.com>
2026-05-27 10:06:51 +0200

    Change trouble nvim configuration

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,7 @@ return {
 			vim.g.gruvbox_material_better_performance = 1
 			vim.g.gruvbox_material_enable_italic = true
 			vim.g.gruvbox_material_enable_bold = true
+			vim.g.gruvbox_material_transparent_background = 1
 			vim.cmd.colorscheme("gruvbox-material")
 
 			vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#d79921" })
