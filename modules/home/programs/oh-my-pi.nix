f926fcf4f04b753e279f9731e50e84843aef6712
emrtnn <emrtnn@proton.me>
2026-09-22 13:51:42 +0200

    Add omp settings

--- modules/home/programs/oh-my-pi.nix
+++ modules/home/programs/oh-my-pi.nix
@@ -3,6 +3,13 @@
 
   programs.omp = {
     enable = true;
-    settings.startup.quiet = true;
+    settings = {
+      startup.quiet = true;
+      setupVersion = 2;
+      theme.dark = "dark-gruvbox";
+      symbolPreset = "unicode";
+      defaultThinkingLevel = "high";
+      edit.mode = "hashline";
+    };
   };
 }
